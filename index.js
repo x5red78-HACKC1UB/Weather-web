3a7624acba9b5d3ee67471cffb9f1ac4deef8b1d
x5red78-HACKC1UB <x5red78@gmail.com>
2026-05-25 13:13:53 +0000

    got a list of every damn weather possible

--- index.js
+++ index.js
@@ -1,3 +1,130 @@
+
+const thunderweatherlist=[
+  "thunderstorm with light rain",
+
+"thunderstorm with rain",
+
+"thunderstorm with heavy rain",
+
+"light thunderstorm",
+
+"thunderstorm",
+
+"heavy thunderstorm",
+
+"ragged thunderstorm",
+
+"thunderstorm with light drizzle",
+
+"thunderstorm with drizzle",
+
+"thunderstorm with heavy drizzle",
+];
+
+const drizzleweatherlist=[
+  "light intensity drizzle",
+
+"drizzle",
+
+"heavy intensity drizzle",
+
+"light intensity drizzle rain",
+
+"drizzle rain",
+
+"heavy intensity drizzle rain",
+
+"shower rain and drizzle",
+
+"heavy shower rain and drizzle",
+
+"shower drizzle"
+];
+
+const rainweatherlist=[
+  "light rain",
+
+"moderate rain",
+
+"heavy intensity rain",
+
+"very heavy rain",
+
+"extreme rain",
+
+"freezing rain",
+
+"light intensity shower rain",
+
+"shower rain",
+
+"heavy intensity shower rain",
+
+"ragged shower rain"
+];
+
+const snowweatherlist=[
+  "light snow",
+
+"snow",
+
+"heavy snow",
+
+"sleet",
+
+"light shower sleet",
+
+"shower sleet",
+
+"light rain and snow",
+
+"rain and snow",
+
+"light shower snow",
+
+"shower snow",
+
+"heavy shower snow"
+];
+
+const atomosphereweatherlist=[
+  "mist",
+
+"smoke",
+
+"haze",
+
+"sand/dust whirls",
+
+"fog",
+
+"sand",
+
+"dust",
+
+"volcanic ash",
+
+"squalls",
+
+"tornado"
+];
+const clearsky=[
+"clear sky"
+];
+const cloudyweatherlist=[
+  "few clouds",
+  "scattered clouds",
+  "broken clouds",
+  "overcast clouds"
+];
+
+
+
+
+
+
+
+
 async function getweather() {
     const city= document.getElementById("city");
    const apikey="66708e4d83f6fb4191ba8b679cdbd3ed";
@@ -11,7 +138,7 @@ let answer= await getdaapikey.json();
     return;
   }
    document.getElementById("weather").innerHTML = `
-    <h2>${answer.name}</h2>
+    <h2 class="Cityname">${answer.name}</h2>
     <p class="Temperature"> ${answer.main.temp}°C</p>
     <p> ${answer.weather[0].description}</p>
     <p> Wind: ${answer.wind.speed} m/s</p>
