f4bb60ba7b950b9806519972c2c9f2f5b4b29c8d
x5red78-HACKC1UB <x5red78@gmail.com>
2026-06-27 20:34:19 +0000

    save

--- server.js
+++ server.js
@@ -1,34 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config(); 
+const express = require("express");
 
 const app = express();
-app.use(cors()); 
 
-
-app.get('/api/weather', async (req, res) => {
-    try {
-        const cityName = req.query.city; 
-        const apiKey = process.env.API_KEY;
-        
-        if (!cityName) {
-            return res.status(400).json({ error: "City name is required" });
-        }
-
-       
-      const apiUrl =
-`https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cityName)}&appid=${apiKey}&units=metric`;
-
-       
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        
-       
-        res.status(response.status).json(data);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
+app.get("/", (req, res) => {
+    res.send("Hello!");
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.get("/api/weather", (req, res) => {
+    res.json({
+        success: true,
+        city: req.query.city
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Listening on http://localhost:3000");
+});
 
